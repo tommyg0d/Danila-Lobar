06b4b06bfcdbd5e33a96a2defcaa65d625765d67
tommyg0d <55397854+tommyg0d@users.noreply.github.com>
2019-09-19 00:07:25 +0300

    Add files via upload

---
+++ my_max.py
@@ -0,0 +1,11 @@
+def maximum(num1, num2):
+    if num1 > num2:
+        return num1 
+    else: return num2 
+    
+a = input('Введите первое число: ')
+b = input('Введите второе число: ')
+
+if a and b:
+    print(maximum(a,b))
+else: print('Введите числа!')
