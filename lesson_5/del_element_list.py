93e19ad741b17b7d29f25c45196ae51f04fe293c
tommyg0d <55397854+tommyg0d@users.noreply.github.com>
2019-10-02 13:10:59 +0300

    Add files via upload

---
+++ lesson_5/del_element_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 13:42:18 2019
+
+@author: danilalobar
+"""
+
+names = ['John','Paul','George','Ringo']
+
+print([x for x in names if x == 'John' or x =='Paul'])
